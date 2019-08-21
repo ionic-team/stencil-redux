ad74bf27c34cb0ef99cb335c4bf9f3c0e14f7131
Edward Silverton <e.silverton@gmail.com>
2019-08-21 11:46:54 +0100

    added default export to store

--- src/global/store.ts
+++ src/global/store.ts
@@ -4,55 +4,57 @@ import { Store } from './interfaces';
 
 declare var Context: any;
 
-Context.store = ((): Store => {
-  let _store: ReduxStore;
+export default () => {
+  Context.store = ((): Store => {
+    let _store: ReduxStore;
 
-  const setStore = (store: ReduxStore) => {
-    _store = store;
-  };
+    const setStore = (store: ReduxStore) => {
+      _store = store;
+    };
 
-  const getState = () => {
-    return _store && _store.getState();
-  };
+    const getState = () => {
+      return _store && _store.getState();
+    };
 
-  const getStore = () => {
-    return _store;
-  };
+    const getStore = () => {
+      return _store;
+    };
 
-  const mapDispatchToProps = (component: any, props: any) => {
-    Object.keys(props).forEach(actionName => {
-      const action = props[actionName];
-      Object.defineProperty(component, actionName, {
-        get: () => (...args: any[]) => _store.dispatch(action(...args)),
-        configurable: true,
-        enumerable: true,
-      });
-    });
-  };
-
-  const mapStateToProps = (component: any, mapState: (...args: any[]) => any) => {
-    // TODO: Don't listen for each component
-    const _mapStateToProps = (_component: any, _mapState: any) => {
-      const mergeProps = mapState(_store.getState());
-      Object.keys(mergeProps).forEach(newPropName => {
-        const newPropValue = mergeProps[newPropName];
-        component[newPropName] = newPropValue;
-        // TODO: can we define new props and still have change detection work?
+    const mapDispatchToProps = (component: any, props: any) => {
+      Object.keys(props).forEach(actionName => {
+        const action = props[actionName];
+        Object.defineProperty(component, actionName, {
+          get: () => (...args: any[]) => _store.dispatch(action(...args)),
+          configurable: true,
+          enumerable: true,
+        });
       });
     };
 
-    const unsubscribe = _store.subscribe(() => _mapStateToProps(component, mapState));
+    const mapStateToProps = (component: any, mapState: (...args: any[]) => any) => {
+      // TODO: Don't listen for each component
+      const _mapStateToProps = (_component: any, _mapState: any) => {
+        const mergeProps = mapState(_store.getState());
+        Object.keys(mergeProps).forEach(newPropName => {
+          const newPropValue = mergeProps[newPropName];
+          component[newPropName] = newPropValue;
+          // TODO: can we define new props and still have change detection work?
+        });
+      };
 
-    _mapStateToProps(component, mapState);
+      const unsubscribe = _store.subscribe(() => _mapStateToProps(component, mapState));
 
-    return unsubscribe;
-  };
+      _mapStateToProps(component, mapState);
 
-  return {
-    getStore,
-    setStore,
-    getState,
-    mapDispatchToProps,
-    mapStateToProps,
-  };
-})();
+      return unsubscribe;
+    };
+
+    return {
+      getStore,
+      setStore,
+      getState,
+      mapDispatchToProps,
+      mapStateToProps,
+    };
+  })();
+};
